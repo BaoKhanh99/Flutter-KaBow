96b89f6febaf13dff3348f6d308b138e667f8c17
BaoKhanh99 <tlbk1999@gmail.com>
2021-05-07 19:22:05 +0700

    add favoritePage

--- lib/Liked_Page/Liked_Page.dart
+++ lib/Liked_Page/Liked_Page.dart
@@ -1,42 +1,56 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:kabow/Colors/ProjectColor.dart';
+import 'package:kabow/Search_Page/LocationItem.dart';
+import 'package:kabow/Search_Page/Searching_form.dart';
 
 class Liked_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-
-        title: Image.asset('assets/logo.png',height: 100,),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-                child: SizedBox(
-                  height: 300,
-                  child: ListView(
-                    children: [
-                      ListTile(
-                        title: Text("data"),
-                      ),
-                      ListTile(
-                        title: Text("data"),
-                      ),
-                      ListTile(
-                        title: Text("data"),
-                      )
-                    ],
-                  ),
-                )
-
-            )
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: BackgroundColor,
+              //expandedHeight: 300,
+              floating: true,
+              pinned: true,
+              toolbarHeight: size.height * 0.11,
+              title: Image.asset(
+                'assets/Images/logo.png',
+                height: size.height * 0.09,
+              ),
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+            ),
+            SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                sliver: LocationList())
           ],
         ),
       ),
     );
+  }
+}
+
+class LocationList extends StatefulWidget {
+  LocationList({Key key}) : super(key: key);
+
+  @override
+  _LocationListState createState() => _LocationListState();
+}
+
+class _LocationListState extends State<LocationList> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (context, index) {
+        return LocationItem();
+      },
+      childCount: 10,
+    ));
   }
 }

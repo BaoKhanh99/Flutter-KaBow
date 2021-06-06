6e9f70664dac8ea30dcc9340ecdbddb837eee9a9
BaoKhanh99 <tlbk1999@gmail.com>
2021-06-07 00:17:01 +0700

    create auth.dart and add FirebaseAuth

---
+++ lib/services/auth.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:kabow/Models/user.dart';
+
+class AuthenService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Users _userFromFirebaseUser(User user) {
+    return user != null ? Users(uid: user.uid) : null;
+  }
+
+  //register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {}
+  }
+
+  //sign in with email and password
+  Future signInEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {}
+  }
+
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {}
+  }
+}
